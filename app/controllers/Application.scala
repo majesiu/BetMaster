f549195232201286809aaeee17a80e9e5f2f6e0e
majesiu <pawel.majewski@mail.com>
2016-04-07 21:49:54 +0200

    Better gameresult type

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,64 @@
 package controllers
 
-import play.api._
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+import java.io.{BufferedWriter, File, FileWriter}
+
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.Form
 import play.api.mvc._
 import play.twirl.api.Html
 
-class Application extends Controller {
+case class MatchDay(date: Date)
 
-  var content = ""
-  val gameresults = models.Scoreboard.getResults("04/03/2016")
-  for(i <- 0 until  gameresults.size by 2){
-    content += ("<div class=\"row\"> <div class=\"col-md-4\">" + gameresults(i).TEAM_ABBREVIATION.getOrElse("")+"</div> <div class=\"col-md-4\"> vs </div><div class=\"col-md-4\">"+gameresults(i+1).TEAM_ABBREVIATION.getOrElse("") + "</div></div>")
-    content += ("<div class=\"row\"> <div class=\"col-md-4\">"+ gameresults(i).sumPoints+"</div> <div class=\"col-md-4\"> </div><div class=\"col-md-4\">"+gameresults(i+1).sumPoints + "</div></div>")
+class Application extends Controller {
+ val MatchDayForm = Form(
+    mapping(
+      "date" -> date
+    )(MatchDay.apply)(MatchDay.unapply)
+  )
+  def search = Action { implicit request =>
+    MatchDayForm.bindFromRequest.fold(
+      formWithErrors => {
+        val html = Html("ups")
+        BadRequest(views.html.scoreboard(html)(MatchDayForm))
+      },
+      matchDayData => {
+        val dateFormat = new SimpleDateFormat("MM-dd-yyyy")
+        val today = dateFormat.format(matchDayData.date)
+        Redirect("/"+today)
+      }
+    )
   }
-  println(gameresults)
+
+
+  def scoreboard2(date: String) = Action {
+    var content = ""
+    val gameresults = models.Scoreboard.getResults(date.replace('-','/'))
+    for(i <- 0 until  gameresults.size by 2){
+      content += ("<div class=\"row\"> <div class=\"col-md-4\">" + gameresults(i).TEAM_ABBREVIATION.getOrElse("")+"</div> <div class=\"col-md-4\"> vs </div><div class=\"col-md-4\">"+gameresults(i+1).TEAM_ABBREVIATION.getOrElse("") + "</div></div>")
+      content += ("<div class=\"row\"> <div class=\"col-md-4\">"+ gameresults(i).sumPoints+"</div> <div class=\"col-md-4\"> </div><div class=\"col-md-4\">"+gameresults(i+1).sumPoints + "</div></div>")
+    }
+    val html = Html(content)
+    Ok(views.html.scoreboard(html)(MatchDayForm))
+  }
 
   def scoreboard = Action {
-    Ok(views.html.scoreboard(Html(content)))
+    val calendar = Calendar.getInstance()
+    calendar.add(Calendar.DAY_OF_MONTH, -1)
+    val dateFormat = new SimpleDateFormat("MM/dd/yyyy")
+    val today = dateFormat.format(calendar.getTime)
+    var content = ""
+    val gameresults = models.Scoreboard.getResults(today)
+    for(i <- 0 until  gameresults.size by 2){
+      content += ("<div class=\"row\"> <div class=\"col-md-4\">" + gameresults(i).TEAM_ABBREVIATION.getOrElse("")+"</div> <div class=\"col-md-4\"> vs </div><div class=\"col-md-4\">"+gameresults(i+1).TEAM_ABBREVIATION.getOrElse("") + "</div></div>")
+      content += ("<div class=\"row\"> <div class=\"col-md-4\">"+ gameresults(i).sumPoints+"</div> <div class=\"col-md-4\"> </div><div class=\"col-md-4\">"+gameresults(i+1).sumPoints + "</div></div>")
+    }
+    val html = Html(content)
+    Ok(views.html.scoreboard(html)(MatchDayForm))
   }
+
+
 
 }
--- app/models/Scoreboard.scala
+++ app/models/Scoreboard.scala
@@ -2,7 +2,9 @@ package models
 
 import java.io.{BufferedWriter, File, FileWriter}
 
-import play.api.libs.json.{JsPath, Json, Reads}
+import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.core.JsonParseException
+import play.api.libs.json.{JsPath, JsValue, Json, Reads}
 import play.api.libs.functional.syntax._
 
 import scalaj.http.Http
@@ -12,33 +14,42 @@ import scalaj.http.Http
   */
 
 object Scoreboard {
+
   case class GameResultRow(rowSet: List[GameResult])
+
   case class Team(name: List[String])
-  case class Available(GAMEID: String, PT_AVAILABLE: Int){
+
+  case class Available(GAMEID: String, PT_AVAILABLE: Int) {
   }
-  case class AvailableRow(name: String, headers: List[String],rowSet: List[Available]){
+
+  case class AvailableRow(name: String, headers: List[String], rowSet: List[Available]) {
   }
-  case class GameResult(GAME_ID: Option[Int], TEAM_ABBREVIATION: Option[String], PTS_QTR1: Option[Int], PTS_QTR2: Option[Int], PTS_QTR3: Option[Int],
-                        PTS_QTR4: Option[Int], PTS_OT1: Option[Int], PTS_OT2: Option[Int], PTS_OT3: Option[Int]){
+
+  case class GameResult(GAME_DATE_EST: Option[String], GAME_SEQUENCE: Option[Int], GAME_ID: Option[String], TEAM_ID: Option[Int], TEAM_ABBREVIATION: Option[String],
+                        TEAM_WINS_LOSSES: Option[String], PTS_QTR1: Option[Int], PTS_QTR2: Option[Int], PTS_QTR3: Option[Int], PTS_QTR4: Option[Int],
+                        PTS_OT1: Option[Int], PTS_OT2: Option[Int], PTS_OT3: Option[Int], PTS_OT4: Option[Int], PTS_OT5: Option[Int], PTS_OT6: Option[Int],
+                        FG_PCT: Option[Double],FT_PCT: Option[Double], FG3_PCT: Option[Double], AST: Option[Int], REB: Option[Int], TOV: Option[Int]) {
     def sumPoints = {
-      PTS_OT1.getOrElse(0)+PTS_OT2.getOrElse(0)+PTS_OT3.getOrElse(0)+PTS_QTR1.getOrElse(0)+PTS_QTR2.getOrElse(0)+PTS_QTR3.getOrElse(0)+PTS_QTR4.getOrElse(0)
+      PTS_OT1.getOrElse(0) + PTS_OT2.getOrElse(0) + PTS_OT3.getOrElse(0) + PTS_QTR1.getOrElse(0) + PTS_QTR2.getOrElse(0) + PTS_QTR3.getOrElse(0) + PTS_QTR4.getOrElse(0) +
+      PTS_OT4.getOrElse(0) + PTS_OT5.getOrElse(0) + PTS_OT6.getOrElse(0)
     }
 
   }
 
   def getScoreboardOnDay(date: String) = {
     val d2 = Http("http://stats.nba.com/stats/scoreboard/")
-      .param("GameDate",date)
-      .param("LeagueID","00")
-      .param("DayOffset","0")
-      .header("user-agent", """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)
+      .param("GameDate", date)
+      .param("LeagueID", "00")
+      .param("DayOffset", "0")
+      .header("user-agent",
+        """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)
                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36""")
-      .header( "referer", "http://stats.nba.com/scores/")
+      .header("referer", "http://stats.nba.com/scores/")
       .asString.toString.split('(')
     val d = d2.drop(1).mkString("(").split('}')
-    val data = d.take(d.length-1).mkString("}").concat("}")
+    val data = d.take(d.length - 1).mkString("}").concat("}")
     val js = Json.parse(data)
-    val file2 = new File("C:\\Users\\majesiu\\Desktop\\games.json")
+    val file2 = new File("I:\\Users\\majesiu\\Desktop\\games.json")
     val bw2 = new BufferedWriter(new FileWriter(file2))
     bw2.write(Json.prettyPrint(js))
     bw2.close()
@@ -48,30 +59,47 @@ object Scoreboard {
   implicit val availableReads: Reads[Available] = (
     JsPath(0).read[String] and
       JsPath(1).read[Int]
-    )(Available.apply _)
+    ) (Available.apply _)
 
   implicit val availableRowReads: Reads[AvailableRow] = (
-    (JsPath  \ "name").read[String] and
-      (JsPath  \ "headers").read[List[String]] and
-      (JsPath  \ "rowSet").read[List[Available]]
-    )(AvailableRow.apply _)
+    (JsPath \ "name").read[String] and
+      (JsPath \ "headers").read[List[String]] and
+      (JsPath \ "rowSet").read[List[Available]]
+    ) (AvailableRow.apply _)
 
   implicit val gameResultReads: Reads[GameResult] = (
-    JsPath(1).readNullable[Int] and
+    JsPath(0).readNullable[String] and
+      JsPath(1).readNullable[Int] and
+      JsPath(2).readNullable[String] and
+      JsPath(3).readNullable[Int] and
       JsPath(4).readNullable[String] and
+      JsPath(6).readNullable[String] and
       JsPath(7).readNullable[Int] and
       JsPath(8).readNullable[Int] and
       JsPath(9).readNullable[Int] and
       JsPath(10).readNullable[Int] and
       JsPath(11).readNullable[Int] and
       JsPath(12).readNullable[Int] and
-      JsPath(13).readNullable[Int]
-    )(GameResult.apply _)
+      JsPath(13).readNullable[Int] and
+      JsPath(14).readNullable[Int] and
+      JsPath(15).readNullable[Int] and
+      JsPath(16).readNullable[Int] and
+      JsPath(22).readNullable[Double] and
+      JsPath(23).readNullable[Double] and
+      JsPath(24).readNullable[Double] and
+      JsPath(25).readNullable[Int] and
+      JsPath(26).readNullable[Int] and
+      JsPath(27).readNullable[Int]
+    ) (GameResult.apply _)
 
   def getResults(date: String) = {
-    ((((getScoreboardOnDay(date) \ "resultSets").get)(1).get) \ "rowSet").as[List[GameResult]]
+    val gameresults = ((((getScoreboardOnDay(date) \ "resultSets").get) (1).get) \ "rowSet").as[List[GameResult]]
+    val file2 = new File("I:\\Users\\majesiu\\Desktop\\gameresults.json")
+    val bw2 = new BufferedWriter(new FileWriter(file2))
+    bw2.write(gameresults.mkString("\n"))
+    bw2.close()
+    gameresults
   }
-
 
 
   /*
